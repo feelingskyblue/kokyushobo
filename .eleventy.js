90fbdf731b6761493792ab29c040552d302aeee2
風野湊 <feelingskyblue@gmail.com>
2023-04-02 22:02:32 +0900

    Markdownの改行設定を変更

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 module.exports = function(eleventyConfig) {
   // Output directory: _site
-
   eleventyConfig.addPassthroughCopy("assets/img");
-
   eleventyConfig.addPassthroughCopy("assets/css");
+};
 
+const markdownIt = require("markdown-it");
+module.exports = function(eleventyConfig) {
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  eleventyConfig.setLibrary("md", markdownIt(options));
 };
