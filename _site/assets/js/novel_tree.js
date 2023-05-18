7c3bab3c1aa4b8d90e11ab025d8e3b8e7dbc654f
風野湊 <feelingskyblue@gmail.com>
2023-05-18 23:52:27 +0900

    目次機能：バージョン1

--- _site/assets/js/novel_tree.js
+++ _site/assets/js/novel_tree.js
@@ -1,8 +1,40 @@
 function novelAnker() {
-  let headingArray = document.querySelectorAll('h3');
-  headingArray.forEach((el, index) => {
+  let heading2Array = document.querySelectorAll('.novel__body h2');
+  heading2Array.forEach((el, index) => {
+    let headingNum = index + 1;
+    el.setAttribute("id", 'sectionMain' + headingNum);
+  });
+
+  let heading3Array = document.querySelectorAll('.novel__body h3');
+  heading3Array.forEach((el, index) => {
     let headingNum = index + 1;
     el.setAttribute("id", 'section' + headingNum);
+  });
+
+  let heading4Array = document.querySelectorAll('.novel__body h4');
+  heading4Array.forEach((el, index) => {
+    let headingNum = index + 1;
+    el.setAttribute("id", 'sectionSub' + headingNum);
+  });
+}
+
+const smoothAnkerScroll = (hash) =>{
+  const targetElement = document.querySelector(hash);
+  const rect = targetElement.getBoundingClientRect().top;
+  const offset = window.pageYOffset;
+  const target = rect + offset;
+  window.scrollTo({
+    top: target,
+    behavior: 'smooth',
+  });
+}
+
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++){
+  smoothScrollTrigger[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = smoothScrollTrigger[i].getAttribute('href');
+    smoothAnkerScroll(href);
   });
 }
 
